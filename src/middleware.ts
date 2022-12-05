4c0a7d262f262bd3f9c7f2b9bf455f7b228325ad
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-12-05 13:01:40 -0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,13 +9,13 @@ export async function middleware(req: NextRequest) {
 
   // Get slug from the pathname of the request URL (e.g. /api/get-url/[slug])
   const slug = req.nextUrl.pathname.split("/").pop();
-  
+
   // Fetch the short link from the database using the slug from the request path name
-  const data = await (
+  const data = await(
     await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
   ).json();
 
-   // Redirect to the long URL of the short link if it exists in the database
+  // Redirect to the long URL of the short link if it exists in the database
   if (data?.url) {
     return NextResponse.redirect(data.url);
   }
